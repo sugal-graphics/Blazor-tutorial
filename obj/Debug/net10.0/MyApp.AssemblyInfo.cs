9f109c4c099ca7bd4d08bb1350dcf30f3e6d8be7
sugal <sugaldarlami@gmail.com>
2026-05-11 11:46:03 +0545

    Counter app using onclick

--- obj/Debug/net10.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d09aa6467aa6fc5ac8fdfbddc146229e155b85e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
