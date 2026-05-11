0be40343b79368fe741aa57855c22fc537c568ff
sugal <sugaldarlami@gmail.com>
2026-05-11 12:08:59 +0545

    To do list app

--- obj/Debug/net10.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f109c4c099ca7bd4d08bb1350dcf30f3e6d8be7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aba92982ec90746ffe719d0d7a5db9d4ecbbf93")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
